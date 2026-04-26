927f8cd46560efe4c33258b3ceda89680f46cda1
sameermansuri09898 <sameermansuri09898@gmail.com>
2026-04-26 18:42:53 +0530

    add moedls

--- Account/models.py
+++ Account/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+class Baseuser(AbstractUser):
+  mobile=models.CharField(max_length=12)
+  is_varified=models.BooleanField(default=False)
+
+
+  def __str__(self):
+    return super().__str__()
+
+class Otp(models.Model):
+  user=models.ForeignKey(Baseuser,on_delete=models.CASCADE)
+  otp=models.CharField(max_length=4)
+  opt_created_at=models.DateTimeField(auto_now_add=True)
+
+
+  def is_expired(self):
+    return self.opt_created_at + timezone.timedelta(minutes=10)<timezone.now()
+  
--- BackendGotel/settings.py
+++ BackendGotel/settings.py
@@ -110,6 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL='Account.Baseuser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
